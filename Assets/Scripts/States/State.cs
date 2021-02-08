dac0daf98e8acb79bee6d3f4fcbaa904af7085a8
Léo Grard <l.grard@student.isartdigital.com>
2021-02-08 19:08:55 +0100

    Add basic State machine class

---
+++ Assets/Scripts/States/State.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+namespace hulaohyes.states
+{
+    public abstract class State
+    {
+        /// Create a new state
+        public State() { }
+
+        public virtual void OnEnter() { }
+        public virtual void LoopLogic() { } 
+        public virtual void PhysLoopLogic() { }
+        public virtual void OnExit() { }
+    }
+}
---
+++ Assets/Scripts/States/StateMachine.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+namespace hulaohyes.states
+{
+    public abstract class StateMachine
+    {
+        private State _currentState;
+
+        ///Create a new state machine object
+        public StateMachine() { }
+
+        ///Get/Set player's current state
+        public State CurrentState
+        {
+            get => _currentState;
+            set
+            {
+                if(_currentState!=null)_currentState.OnExit();
+                _currentState = value;
+                _currentState.OnEnter();
+            }
+        }
+    }
+}
