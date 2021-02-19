1c6a2972d1617a988f46b7ad26a66956a87d657c
Léo Grard <l.grard@student.isartdigital.com>
2021-02-19 17:43:46 +0100

    Add Effects script hitstop

---
+++ Assets/Scripts/Effects/Effects.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace hulaohyes.effects
+{
+    public class Effects
+    {
+        public static IEnumerator HitStop(Animator pPlayerAnimator, Animator pTargetAnimator, float pDuration, float pSlowAmount)
+        {
+            pPlayerAnimator.speed = pSlowAmount;
+            pTargetAnimator.speed = pSlowAmount;
+            yield return new WaitForSeconds(pDuration);
+            pPlayerAnimator.speed = 1;
+            pTargetAnimator.speed = 1;
+        }
+
+        public static IEnumerator ScreenShake(float pMagnitude, float pDuration, Transform pCamera)
+        {
+            //Set camera shake
+            yield return new WaitForSeconds(pDuration);
+            //disable camera shake
+        }
+    }
+}
