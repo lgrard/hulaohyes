45980b94fbae600fd36f8b86277526c71a48f667
leo grard <l.grard@student.isartdigital.com>
2021-02-27 10:38:52 +0100

    Add editor HulaOhYes tab preset

---
+++ Assets/Scripts/Editor/AddCustomTab.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class AddCustomTab : MonoBehaviour
+{
+    [MenuItem("HulaOhYes/Player")] static void newPlayer() => newItemFromPrefab("Prefabs/Player");
+    [MenuItem("HulaOhYes/Managers/GameManager")] static void newGameManager() => newItemFromPrefab("Prefabs/Managers/GameManager");
+    [MenuItem("HulaOhYes/Managers/CameraManager")] static void newCameraManager() => newItemFromPrefab("Prefabs/Managers/CameraManager");
+    [MenuItem("HulaOhYes/Cameras/Look At Camera")] static void newLookAtCamera() => newItemFromPrefab("Prefabs/Cameras/LookAtCamera");
+    [MenuItem("HulaOhYes/Cameras/Angled Camera")] static void newAngledCamera() => newItemFromPrefab("Prefabs/Cameras/AngledCamera");
+
+
+    static void newItemFromPrefab(string pFilePath)
+    {
+        Object lItem = Resources.Load(pFilePath);
+        if (lItem != null) PrefabUtility.InstantiatePrefab(lItem);
+        else Debug.LogError(pFilePath + " is not a valid pathname");
+    }
+}
