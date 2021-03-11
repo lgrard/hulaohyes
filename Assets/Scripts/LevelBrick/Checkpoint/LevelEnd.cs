c5f18b77fc476122be784921b5e83f08f48d8331
leo grard <l.grard@student.isartdigital.com>
2021-03-11 16:52:10 +0100

    Add level end

---
+++ Assets/Scripts/LevelBrick/Checkpoint/LevelEnd.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using hulaohyes.player;
+
+namespace hulaohyes.levelbrick.checkpoint
+{
+    public class LevelEnd : MonoBehaviour
+    {
+        private bool player0in = false;
+        private bool player1in = false;
+
+        [Header("Values")]
+        [SerializeField] private Vector3 triggerZoneSize;
+
+        [SerializeField] bool drawGizmos = true;
+
+        private void Start()
+        {
+            BoxCollider lTrigger = gameObject.AddComponent<BoxCollider>();
+            lTrigger.isTrigger = true;
+            lTrigger.size = triggerZoneSize;
+            lTrigger.center = new Vector3(0, lTrigger.size.y/2, 0);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.TryGetComponent<PlayerController>(out PlayerController lPlayer))
+            {
+                if (lPlayer.playerIndex == 0) player0in = true;
+                else player1in = true;
+            }
+
+            if (player0in && player1in) Debug.Log("Win !");
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.TryGetComponent<PlayerController>(out PlayerController lPlayer))
+            {
+                if (lPlayer.playerIndex == 0) player0in = false;
+                else player1in = false;
+            }
+
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (drawGizmos)
+            {
+                Gizmos.color = Color.blue;
+                Gizmos.matrix = Matrix4x4.TRS(transform.position,transform.rotation,transform.lossyScale);
+                Gizmos.DrawWireCube(Vector3.zero + new Vector3(0, triggerZoneSize.y/2, 0), triggerZoneSize);
+            }
+        }
+    }
+}
--- Assets/Scripts/LevelBrick/Checkpoint/PlayerStart.cs
+++ Assets/Scripts/LevelBrick/Checkpoint/PlayerStart.cs
@@ -12,6 +12,8 @@ namespace hulaohyes.levelbrick.checkpoint
         [SerializeField] private Transform _spawnPosition;
         [SerializeField] private Transform _secondSpawnPosition;
 
+        [SerializeField] bool drawGizmos =true;
+
         private void Awake()
         {
             if (_instance == null) _instance = this;
@@ -42,17 +44,19 @@ namespace hulaohyes.levelbrick.checkpoint
         private void OnDrawGizmos()
         {
             Gizmos.color = Color.blue;
-
-            if (_spawnPosition != null)
+            if (drawGizmos)
             {
-                Gizmos.DrawSphere(_spawnPosition.forward * 0.5f + _spawnPosition.position, 0.25f);
-                Gizmos.DrawSphere(_spawnPosition.position, 0.5f);
-            }
+                if (_spawnPosition != null)
+                {
+                    Gizmos.DrawSphere(_spawnPosition.forward * 0.5f + _spawnPosition.position, 0.25f);
+                    Gizmos.DrawSphere(_spawnPosition.position, 0.5f);
+                }
 
-            if (_secondSpawnPosition != null)
-            {
-                Gizmos.DrawSphere(_secondSpawnPosition.forward * 0.5f + _secondSpawnPosition.position, 0.25f);
-                Gizmos.DrawSphere(_secondSpawnPosition.position, 0.5f);
+                if (_secondSpawnPosition != null)
+                {
+                    Gizmos.DrawSphere(_secondSpawnPosition.forward * 0.5f + _secondSpawnPosition.position, 0.25f);
+                    Gizmos.DrawSphere(_secondSpawnPosition.position, 0.5f);
+                }
             }
         }
     }
