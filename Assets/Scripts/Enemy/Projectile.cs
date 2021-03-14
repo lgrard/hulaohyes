bbfe38251db6a9a3c3b3378d4dfecc6d8e08273f
leo grard <l.grard@student.isartdigital.com>
2021-03-14 14:22:01 +0100

    Add timer to projectile

--- Assets/Scripts/Enemy/Projectile.cs
+++ Assets/Scripts/Enemy/Projectile.cs
@@ -7,17 +7,23 @@ namespace hulaohyes.enemy
 {
     public class Projectile : MonoBehaviour
     {
-        private const float PROJECTILE_SPEED = 5f;
+        public float PROJECTILE_LIFETIME = 3f;          //const to change
+        public float PROJECTILE_SPEED = 5f;       //const to change
         private Rigidbody rb;
+        private float destroyTimer;
 
         private void Start()
         {
             rb = GetComponent<Rigidbody>();
+            destroyTimer = PROJECTILE_LIFETIME;
         }
 
         private void FixedUpdate()
         {
             rb.velocity = transform.forward * PROJECTILE_SPEED;
+
+            if (destroyTimer > 0) destroyTimer -= Time.deltaTime;
+            else Destroy(gameObject);
         }
 
         private void OnTriggerEnter(Collider other)
