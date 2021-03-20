26fc57507896492eb60b387b914f26c80dd9454c
leo grard <l.grard@student.isartdigital.com>
2021-03-20 18:25:23 +0100

    Add analyze requirements tab in content manager

--- Assets/Scripts/Editor/ContentManagerWindow.cs
+++ Assets/Scripts/Editor/ContentManagerWindow.cs
@@ -4,7 +4,12 @@ using UnityEditor;
 class ContentManagerWindow : EditorWindow
 {
     private Material headerMat;
-    private Texture headerImage; 
+    private Texture headerImage;
+
+    private string[] tabs = { "Add", "Analyze" };
+    private int currentTab = 0;
+    private GUIStyle tabStyle;
+
     private float buttonWidth;
     private float buttonHeight = 35;
     private float labelWidth;
@@ -17,6 +22,12 @@ class ContentManagerWindow : EditorWindow
     private GameObject lastObject;
     private string overridenObjectName;
     private Editor objectEditor;
+
+    private Object hasGameManager;
+    private Object hasCamManager;
+    private Object hasPlayerStart;
+    private Object hasPlayer0;
+    private Object hasPlayer1;
 
     [MenuItem("HulaOhYes/Content Manager &HOME")] private static void ShowWindow() => EditorWindow.GetWindow(typeof(ContentManagerWindow)).titleContent.text = "Content Manager";
 
@@ -42,6 +53,8 @@ class ContentManagerWindow : EditorWindow
         labelStyle.fontStyle = FontStyle.Italic;
         labelStyle.alignment = TextAnchor.MiddleRight;
         labelStyle.padding.right = 10;
+        
+        tabStyle = new GUIStyle(EditorStyles.miniButton);
 
         headerImage = (Texture)Resources.Load("Editor/CM_header");
         headerMat = (Material)Resources.Load("Editor/M_EditorSprite");
@@ -60,128 +73,172 @@ class ContentManagerWindow : EditorWindow
         buttonWidth = position.width / 1.5f;
         labelWidth = position.width / 4f;
 
-        if(headerImage != null && position.height> 400)
-        EditorGUI.DrawPreviewTexture(GUILayoutUtility.GetRect(256, 50), headerImage, headerMat, ScaleMode.ScaleAndCrop);
+        if (headerImage != null && position.height > 400)
+            EditorGUI.DrawPreviewTexture(GUILayoutUtility.GetRect(256, 50), headerImage, headerMat, ScaleMode.ScaleAndCrop);
 
         GUILayout.Space(15);
         GUILayout.Label("Content Manager", titleStyle);
+
+        GUILayout.Space(15);
+        GUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+        currentTab = GUILayout.Toolbar(currentTab, tabs, tabStyle,GUI.ToolbarButtonSize.Fixed, GUILayout.Width(200));
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
         GUILayout.Space(15);
 
         scrollView = GUILayout.BeginScrollView(scrollView);
-
-        GUILayout.BeginHorizontal();
-        GUILayout.Label("Players", labelStyle, GUILayout.Width(labelWidth));
-        if (GUILayout.Button("Player_0", GUILayout.Width(buttonWidth/2), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Player_0");
-        if(GUILayout.Button("Player_1", GUILayout.Width(buttonWidth/2), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Player_1");
-        GUILayout.EndHorizontal();
-
-        GUILayout.Space(5f);
-
-        GUILayout.BeginHorizontal();
-        GUILayout.Label("Managers", labelStyle, GUILayout.Width(labelWidth));
-        if (GUILayout.Button("GameManager", GUILayout.Width(buttonWidth / 2), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Managers/GameManager");
-        if (GUILayout.Button("CameraManager", GUILayout.Width(buttonWidth / 2), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Managers/CameraManager");
-        GUILayout.EndHorizontal();
-
-        GUILayout.Space(5f);
-
-        GUILayout.BeginHorizontal();
-        GUILayout.Label("Cameras", labelStyle, GUILayout.Width(labelWidth));
-        if (GUILayout.Button("LookAtCamera", GUILayout.Width(buttonWidth / 2), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Cameras/LookAtCamera");
-        if (GUILayout.Button("AngledCamera", GUILayout.Width(buttonWidth / 2), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Cameras/AngledCamera");
-        GUILayout.EndHorizontal();
-
-        GUILayout.Space(5f);
-
-        GUILayout.BeginHorizontal();
-        GUILayout.Label("Enemies", labelStyle, GUILayout.Width(labelWidth));
-        if (GUILayout.Button("Walker", GUILayout.Width(buttonWidth / 2), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Enemy/Enemy_Walker");
-        if (GUILayout.Button("Turret", GUILayout.Width(buttonWidth / 2), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Enemy/Enemy_Turret");
-        GUILayout.EndHorizontal();
-
-        GUILayout.Space(5f);
-
-        GUILayout.BeginHorizontal();
-        GUILayout.Label("Bricks", labelStyle, GUILayout.Width(labelWidth));
-        if (GUILayout.Button("DoorGroup", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Bricks/DoorGroup");
-        if (GUILayout.Button("UnitCubeSpawner", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Bricks/UnitCubeSpawner");
-        if (GUILayout.Button("Barrel", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Bricks/Barrel");
-        GUILayout.EndHorizontal();
-
-        GUILayout.Space(5f);
-
-        GUILayout.BeginHorizontal();
-        GUILayout.Label("Gears", labelStyle, GUILayout.Width(labelWidth));
-        if (GUILayout.Button("Gear", GUILayout.Width(buttonWidth), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Bricks/Gear");
-        GUILayout.EndHorizontal();
-
-        GUILayout.Space(5f);
-
-        GUILayout.BeginHorizontal();
-        GUILayout.Label("Checkpoints", labelStyle, GUILayout.Width(labelWidth));
-        if (GUILayout.Button("PlayerStart", GUILayout.Width(buttonWidth / 4), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Bricks/Checkpoints/PlayerStart");
-        if (GUILayout.Button("Checkpoint", GUILayout.Width(buttonWidth / 4), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Bricks/Checkpoints/Checkpoint");
-        if (GUILayout.Button("BigCheckpoint", GUILayout.Width(buttonWidth / 4), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Bricks/Checkpoints/BigCheckpoint");
-        if (GUILayout.Button("LevelEnd", GUILayout.Width(buttonWidth / 4), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Bricks/Checkpoints/LevelEnd");
-        GUILayout.EndHorizontal();
-
-        GUILayout.Space(30f);
-
-        GUILayout.BeginHorizontal();
-        GUILayout.Label("Fence", labelStyle, GUILayout.Width(labelWidth));
-        if (GUILayout.Button("x0", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/LD/SM_Fence_x0");
-        if (GUILayout.Button("x1", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/LD/SM_Fence_x1");
-        if (GUILayout.Button("x2", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/LD/SM_Fence_x2");
-        GUILayout.EndHorizontal();
-        GUILayout.Space(5f);
-        GUILayout.BeginHorizontal();
-        GUILayout.Label("Outline", labelStyle, GUILayout.Width(labelWidth));
-        if (GUILayout.Button("x0", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/LD/SM_Outline_x0");
-        if (GUILayout.Button("x1", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/LD/SM_Outline_x1");
-        if (GUILayout.Button("x2", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/LD/SM_Outline_x2");
-        GUILayout.EndHorizontal();
-        GUILayout.Space(5f);
-        GUILayout.BeginHorizontal();
-        GUILayout.Label("Plank", labelStyle, GUILayout.Width(labelWidth));
-        if (GUILayout.Button("x1", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/LD/SM_Plank_x1");
-        if (GUILayout.Button("x2", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/LD/SM_Plank_x2");
-        if (GUILayout.Button("x4", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/LD/SM_Plank_x4");
-        GUILayout.EndHorizontal();
-        GUILayout.Space(5f);
-        GUILayout.BeginHorizontal();
-        GUILayout.Label("Pole", labelStyle, GUILayout.Width(labelWidth));
-        if (GUILayout.Button("x1", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/LD/SM_Pole_x1");
-        if (GUILayout.Button("x2", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/LD/SM_Pole_x2");
-        if (GUILayout.Button("x4", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/LD/SM_Pole_x4");
-        GUILayout.EndHorizontal();
-
-
-        GUILayout.EndScrollView();
-
-        GUILayout.Space(15f);
-        if (objectToLoad != null)
+        
+        //Add selected
+        if (currentTab == 0)
         {
+
             GUILayout.BeginHorizontal();
-            GUILayout.Label("Object name",labelStyle, GUILayout.Width(position.width / 4));
-            overridenObjectName = GUILayout.TextField(overridenObjectName, GUILayout.Width(position.width/1.5f));
+            GUILayout.Label("Players", labelStyle, GUILayout.Width(labelWidth));
+            if (GUILayout.Button("Player_0", GUILayout.Width(buttonWidth / 2), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Player_0");
+            if (GUILayout.Button("Player_1", GUILayout.Width(buttonWidth / 2), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Player_1");
             GUILayout.EndHorizontal();
-            
+
             GUILayout.Space(5f);
 
-            GUILayout.Label("Click anywhere to create a new " + objectToLoad.name, indicationStyle);
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Managers", labelStyle, GUILayout.Width(labelWidth));
+            if (GUILayout.Button("GameManager", GUILayout.Width(buttonWidth / 2), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Managers/GameManager");
+            if (GUILayout.Button("CameraManager", GUILayout.Width(buttonWidth / 2), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Managers/CameraManager");
+            GUILayout.EndHorizontal();
+
+            GUILayout.Space(5f);
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Cameras", labelStyle, GUILayout.Width(labelWidth));
+            if (GUILayout.Button("LookAtCamera", GUILayout.Width(buttonWidth / 2), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Cameras/LookAtCamera");
+            if (GUILayout.Button("AngledCamera", GUILayout.Width(buttonWidth / 2), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Cameras/AngledCamera");
+            GUILayout.EndHorizontal();
+
+            GUILayout.Space(5f);
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Enemies", labelStyle, GUILayout.Width(labelWidth));
+            if (GUILayout.Button("Walker", GUILayout.Width(buttonWidth / 2), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Enemy/Enemy_Walker");
+            if (GUILayout.Button("Turret", GUILayout.Width(buttonWidth / 2), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Enemy/Enemy_Turret");
+            GUILayout.EndHorizontal();
+
+            GUILayout.Space(5f);
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Bricks", labelStyle, GUILayout.Width(labelWidth));
+            if (GUILayout.Button("DoorGroup", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Bricks/DoorGroup");
+            if (GUILayout.Button("UnitCubeSpawner", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Bricks/UnitCubeSpawner");
+            if (GUILayout.Button("Barrel", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Bricks/Barrel");
+            GUILayout.EndHorizontal();
+
+            GUILayout.Space(5f);
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Gears", labelStyle, GUILayout.Width(labelWidth));
+            if (GUILayout.Button("Gear", GUILayout.Width(buttonWidth), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Bricks/Gear");
+            GUILayout.EndHorizontal();
+
+            GUILayout.Space(5f);
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Checkpoints", labelStyle, GUILayout.Width(labelWidth));
+            if (GUILayout.Button("PlayerStart", GUILayout.Width(buttonWidth / 4), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Bricks/Checkpoints/PlayerStart");
+            if (GUILayout.Button("Checkpoint", GUILayout.Width(buttonWidth / 4), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Bricks/Checkpoints/Checkpoint");
+            if (GUILayout.Button("BigCheckpoint", GUILayout.Width(buttonWidth / 4), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Bricks/Checkpoints/BigCheckpoint");
+            if (GUILayout.Button("LevelEnd", GUILayout.Width(buttonWidth / 4), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/Bricks/Checkpoints/LevelEnd");
+            GUILayout.EndHorizontal();
+
+            GUILayout.Space(30f);
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Fence", labelStyle, GUILayout.Width(labelWidth));
+            if (GUILayout.Button("x0", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/LD/SM_Fence_x0");
+            if (GUILayout.Button("x1", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/LD/SM_Fence_x1");
+            if (GUILayout.Button("x2", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/LD/SM_Fence_x2");
+            GUILayout.EndHorizontal();
+            GUILayout.Space(5f);
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Outline", labelStyle, GUILayout.Width(labelWidth));
+            if (GUILayout.Button("x0", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/LD/SM_Outline_x0");
+            if (GUILayout.Button("x1", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/LD/SM_Outline_x1");
+            if (GUILayout.Button("x2", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/LD/SM_Outline_x2");
+            GUILayout.EndHorizontal();
+            GUILayout.Space(5f);
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Plank", labelStyle, GUILayout.Width(labelWidth));
+            if (GUILayout.Button("x1", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/LD/SM_Plank_x1");
+            if (GUILayout.Button("x2", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/LD/SM_Plank_x2");
+            if (GUILayout.Button("x4", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/LD/SM_Plank_x4");
+            GUILayout.EndHorizontal();
+            GUILayout.Space(5f);
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Pole", labelStyle, GUILayout.Width(labelWidth));
+            if (GUILayout.Button("x1", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/LD/SM_Pole_x1");
+            if (GUILayout.Button("x2", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/LD/SM_Pole_x2");
+            if (GUILayout.Button("x4", GUILayout.Width(buttonWidth / 3), GUILayout.Height(buttonHeight))) LoadObject("Prefabs/LD/SM_Pole_x4");
+            GUILayout.EndHorizontal();
+
+
+            GUILayout.EndScrollView();
+
+            GUILayout.Space(15f);
+            if (objectToLoad != null)
+            {
+                GUILayout.BeginHorizontal();
+                GUILayout.Label("Object name", labelStyle, GUILayout.Width(position.width / 4));
+                overridenObjectName = GUILayout.TextField(overridenObjectName, GUILayout.Width(position.width / 1.5f));
+                GUILayout.EndHorizontal();
+
+                GUILayout.Space(5f);
+
+                GUILayout.Label("Click anywhere to create a new " + objectToLoad.name, indicationStyle);
+            }
+            else GUILayout.Label("Select an object", indicationStyle);
+
+            GUILayout.Space(15f);
+            if (lastObject != null)
+                if (GUILayout.Button("Undo", GUILayout.Height(buttonHeight))) Undo();
+            GUILayout.Space(5f);
+
+            if (objectToLoad != null)
+            {
+                if (objectEditor == null)
+                    objectEditor = Editor.CreateEditor(objectToLoad);
+                objectEditor.OnInteractivePreviewGUI(GUILayoutUtility.GetRect(256, 150), new GUIStyle());
+            }
         }
-        else GUILayout.Label("Select an object", indicationStyle);
 
-        GUILayout.Space(15f);
-        if (lastObject != null)
-            if (GUILayout.Button("Undo", GUILayout.Height(buttonHeight))) Undo();
-        GUILayout.Space(5f);
-
-        if (objectToLoad != null)
+        //Analyze selected
+        else if (currentTab == 1)
         {
-            if(objectEditor == null)
-                objectEditor = Editor.CreateEditor(objectToLoad);
-            objectEditor.OnInteractivePreviewGUI(GUILayoutUtility.GetRect(256, 150), new GUIStyle());
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            GUILayout.BeginVertical();
+
+            GUILayout.Space(5);
+            EditorGUILayout.ObjectField("GameManager", hasGameManager, typeof(GameObject),true);
+            EditorGUILayout.ObjectField("CameraManager", hasCamManager, typeof(GameObject),true);
+            EditorGUILayout.ObjectField("PlayerStart", hasPlayerStart, typeof(GameObject),true);
+            EditorGUILayout.ObjectField("Player0", hasPlayer0, typeof(GameObject),true);
+            EditorGUILayout.ObjectField("Player1", hasPlayer1, typeof(GameObject),true);
+            /*GUILayout.Toggle(hasCamManager, "CameraManager");
+            GUILayout.Toggle(hasPlayerStart, "PlayerStart");
+            GUILayout.Toggle(hasPlayer0, "Player0");
+            GUILayout.Toggle(hasPlayer1, "Player1");*/
+
+            GUILayout.EndVertical();
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+
+            GUILayout.Space(15);
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button("Analyze current scene",GUILayout.Width(200), GUILayout.Height(50))) AnalyzeScene();
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+
+            GUILayout.EndScrollView();
         }
     }
 
@@ -222,5 +279,14 @@ class ContentManagerWindow : EditorWindow
     {
         DestroyImmediate(lastObject);
         lastObject = null;
+    }
+
+    void AnalyzeScene()
+    {
+        hasGameManager = GameObject.Find("GameManager");
+        hasCamManager = GameObject.Find("CameraManager");
+        hasPlayerStart = GameObject.Find("PlayerStart");
+        hasPlayer0 = GameObject.Find("Player_0");
+        hasPlayer1 = GameObject.Find("Player_1");
     }
 }
